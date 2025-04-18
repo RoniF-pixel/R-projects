f199e7986927d112854521894b4e8ebd372adb09
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2025-04-18 12:36:57 +0330

    step1

--- Cognitive Load & Decision-Making Using Reaction Time Data/Cognitive Load & Decision-Making.R
+++ Cognitive Load & Decision-Making Using Reaction Time Data/Cognitive Load & Decision-Making.R
@@ -1,1 +1,12 @@
-"New Project"
+"cognitive "
+#libraries
+library(tidyverse)
+library(ggplot2)
+
+#the dataset
+# Load dataset directly from GitHub
+setwd("C:/Users/0&1/OneDrive/Documents/R-projects/R-projects/Cognitive Load & Decision-Making Using Reaction Time Data")
+stroop_data = read.csv("stroop.csv")
+
+# Check structure
+glimpse(stroop_data)
