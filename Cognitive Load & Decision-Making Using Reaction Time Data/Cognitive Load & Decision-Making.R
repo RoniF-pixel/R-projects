365955f6496df7f02d8905647dc2a8bad2c734a8
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2025-04-18 15:25:17 +0330

    step2, data cleaning

--- Cognitive Load & Decision-Making Using Reaction Time Data/Cognitive Load & Decision-Making.R
+++ Cognitive Load & Decision-Making Using Reaction Time Data/Cognitive Load & Decision-Making.R
@@ -10,3 +10,18 @@ stroop_data = read.csv("stroop.csv")
 
 # Check structure
 glimpse(stroop_data)
+
+
+# The dataset includes average RTs per person for congruent and incongruent trials
+# Convert to long format for analysis
+
+stroop_long <- stroop_data %>%
+  pivot_longer(cols = c(Congruent, Incongruent),
+               names_to = "condition",
+               values_to = "rt_sec")
+
+# Convert seconds to milliseconds
+stroop_long <- stroop_long %>%
+  mutate(rt_ms = rt_sec * 1000,
+         condition = factor(condition, levels = c("Congruent", "Incongruent")))
+
